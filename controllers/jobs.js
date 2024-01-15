bd4928581c1f48bcba14158229ce86f01dea917f
tkhan0023 <kardosepeter@gmail.com>
2024-01-15 19:28:15 +0530

    first commit

---
+++ controllers/jobs.js
@@ -0,0 +1,64 @@
+const Job = require('../models/Job');
+const {StatusCodes} =require('http-status-codes')
+const {BadRequestError,NotFoundError} = require('../errors')
+const getAllJobs= async (req,res) =>{
+   const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
+  res.status(StatusCodes.OK).json({jobs,count:jobs.length})
+}
+
+const getJob= async (req, res) => {
+    // res.send('get Single Job');
+    const {user:{userId},params:{id:jobId}} = req
+    const job = await Job.findOne({
+        _id:jobId,createdBy:userId
+    })
+if(!job){
+    throw new  NotFoundError(`Job ${jobId} not found`)
+}
+res.status(StatusCodes.OK).json({job})
+}
+const createJob= async (req,res) =>{
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
+   
+}
+const updateJob= async (req,res) =>{
+    const {
+        body:{company,position},
+        user:{userId},
+        params:{id:jobId}
+    }=req
+    if(company === '' || position ===''){
+        throw new BadRequestError('company or position field cannot be empty')
+    }
+    const job = await Job.findOneAndUpdate(
+        {_id:jobId,createdBy:userId},
+    req.body,{new: true,reunValidators:true}
+    )
+    if(!job){
+        throw new  NotFoundError(`Job ${jobId} not found`)
+    }
+    res.status(StatusCodes.OK).json({job})
+}
+const deleteJob= async (req,res) =>{
+    const {
+        user:{userId},
+        params:{id:jobId}
+    }=req
+    const job = await Job.findByIdAndRemove({
+        _id:jobId,createdBy:userId
+    })
+    if(!job){
+        throw new  NotFoundError(`Job ${jobId} not found`)
+    }
+    res.status(StatusCodes.OK).send()
+}
+module.exports = {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob
+    
+}
